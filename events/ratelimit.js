1a92c6e8044da452405d1ed014c75eda83d32f54
VenomousSteam81 <67353322+VenomousSteam81@users.noreply.github.com>
2022-09-11 23:13:53 +0000

    Made the handler post when WebManageBot hits a ratelimit

--- events/ratelimit.js
+++ events/ratelimit.js
@@ -1,5 +1,11 @@
 const client = require('..');
 
+const Errorhandler = require('discord-error-handler');
+const handle = new Errorhandler(client, {
+	webhook: { id: process.env.webhook_id, token: process.env.webhook_token },
+});
+
 client.on("rateLimit", data => {
-  if (data.timeout > 1000) process.kill(1);
+  if (data.timeout > 1000) process.kill(1),
+	handle.createrr("WebManageBot hit a ratelimit!", data.timeout, date.now);
 });
