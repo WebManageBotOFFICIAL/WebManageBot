3575ea05efb8e73d504fe2db24b2ec8f1c98e945
VenomousSteam81 <67353322+VenomousSteam81@users.noreply.github.com>
2022-09-14 19:19:23 +0000

    WebManageBot wasn't recovering from ratelimits, so fixed that part

--- events/ratelimit.js
+++ events/ratelimit.js
@@ -1,21 +1,5 @@
 const client = require('..');
-const Discord = require('discord.js');
-const webhookClient = new Discord.WebhookClient({ url: process.env.ratelimitWebhook_url });
 
 client.on("rateLimit", data => {
-	const embed = new Discord.MessageEmbed()
-		.setTitle('Ratelimit')
-		.setDescription('WebManageBot hit a ratelimit!')
-		.addFields(
-			{ name: 'Date and Time', value: `${Date.now}`, inline: true },
-			{ name: 'Data Timeout', value: `${data.timeout}`, inline: true },
-		)
-		.setColor(random);
-	
-  if (data.timeout > 1000) process.kill(1),
-		webhookClient.send({
-			content: ``,
-			username: 'Ratelimit',
-			embeds: [embed],
-	});
+  if (data.timeout > 1000) process.kill(1);
 });
