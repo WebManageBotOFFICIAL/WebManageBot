d2b1f6fc88aeb70cf5c980801101e19bd14ab074
VenomousSteam81 <jordanjulian.stu@joplinschools.org>
2022-10-04 12:20:24 -0500

    fixing a package, commit #8

--- commands/fun/buttoncalc.js
+++ commands/fun/buttoncalc.js
@@ -1,12 +1,13 @@
 const Discord = module.require("discord.js");
-const simplydjs = require("simply-djs")
+//const simplydjs = require("simply-djs")
 
 module.exports = {
   name: "bcalc",
   description: "Calculator with buttons",
   run: async (client, message, args) => {
-    simplydjs.calculator(message, {
+    /* simplydjs.calculator(message, {
     	embedColor: '#075FFF',
-    })
+    }) */
+    message.reply('This currently does not work due to an npm package issue')
   },
 };
--- commands/games/ttt.js
+++ commands/games/ttt.js
@@ -1,11 +1,12 @@
 const Discord = module.require("discord.js");
-const simplydjs = require("simply-djs")
+//const simplydjs = require("simply-djs")
 
 module.exports = {
   name: "ttt",
 	aliases: "tictactoe",
   description: "Tic Tac Toe",
   run: async (client, message, args) => {
-    simplydjs.tictactoe(message)
+    //simplydjs.tictactoe(message)
+    message.reply('This currently does not work due to an npm package issue')
   },
 };
