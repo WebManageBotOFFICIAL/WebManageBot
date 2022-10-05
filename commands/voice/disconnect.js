6ed9aa24c513a19cb043baf61e6038057a8e9d59
VenomousSteam81 <jordanjulian.stu@joplinschools.org>
2022-10-05 14:26:06 -0500

    fix another thing

--- commands/voice/disconnect.js
+++ commands/voice/disconnect.js
@@ -7,7 +7,9 @@ module.exports = {
 	aliases: "dc",
 	run: async (client, message, args) => {
         const { getVoiceConnection } = require('@discordjs/voice');
+        const myVoiceChannel = args[1];
         const connection = getVoiceConnection(myVoiceChannel.guild.id);
+        if (!args[1]) message.reply('Please provide the voice channel ID')
     try {
         connection.destroy();
     } catch (error) {
