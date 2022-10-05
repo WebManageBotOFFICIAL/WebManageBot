3b718178b1775ac49ce7466cca8e62e2afa699d8
VenomousSteam81 <jordanjulian.stu@joplinschools.org>
2022-10-05 14:19:41 -0500

    fix `connection is not defined`

--- commands/voice/play.js
+++ commands/voice/play.js
@@ -6,13 +6,13 @@ module.exports = {
 	description: "Join a given VC",
 	aliases: "connect",
 	run: async (client, message, args) => {
+        const connection = joinVoiceChannel({
+            channelId: channel.id,
+            guildId: channel.guild.id,
+            adapterCreator: channel.guild.voiceAdapterCreator,
+        })
         const subscription = connection.subscribe(audioPlayer);
         try {
-            const connection = joinVoiceChannel({
-                channelId: channel.id,
-                guildId: channel.guild.id,
-                adapterCreator: channel.guild.voiceAdapterCreator,
-            })
             if (subscription) {
                 // Unsubscribe after 5 seconds (stop playing audio on the voice connection)
                 setTimeout(() => subscription.unsubscribe(), 5_000);
