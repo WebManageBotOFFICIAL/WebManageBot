5e1af147d05fd3a9a2092018c59dd67893bc720a
VenomousSteam81 <VenomousSteam81@outlook.com>
2023-02-08 15:22:17 -0600

    Add @everyone protection
    Hopefully it won't ever ping now

--- commands/fun/say.js
+++ commands/fun/say.js
@@ -3,10 +3,13 @@ module.exports = {
 	description: "Make me repeat what you want me to say",
 	usage: "say <message>",
 		run: async (client, message, args) => {
-			const command = args.join(" ");
-			if (!command) return message.reply('There were no commands given!');
+			const say = args.join(" ");
+			if (!say) return message.reply('There were no commands given!');
+			if (say.includes("@everyone")) {
+  				message.reply("The string contains '@everyone'. Please don't try to do that!");
+			}
     try {
-			message.channel.send(command);
+			message.channel.send(say);
 		} catch (error) {
 			console.error(error);
 			message.channel.send(error);
