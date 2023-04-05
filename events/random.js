f64ef409a5b30db24005af5c59b22397f4282f91
VenomousSteam81 <VenomousSteam81@outlook.com>
2023-04-05 11:55:29 -0500

    Create random.js

---
+++ events/random.js
@@ -0,0 +1,6 @@
+client.on('ready', () => {
+ setInterval(() => {
+   const randomMessage = Math.floor(Math.random() * messages.length);
+   client.channels.cache.get("1093216937938522113").send(messages[randomMessage]);
+ }, 1000* 60* 60) // 1 hour
+});
